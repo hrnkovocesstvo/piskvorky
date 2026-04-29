75540a23129d628d2b0531eca8de6e332d4bbb7d
hrnkovocesstvopcdole <BabnicA.itb25@vsps-su.cz>
2026-04-29 08:36:53 +0200

    quick1

--- piskvorky/Form1.Designer.cs
+++ piskvorky/Form1.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.numericUpDownRow = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownWin = new System.Windows.Forms.NumericUpDown();
             this.buttonStart = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRow)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWin)).BeginInit();
             this.SuspendLayout();
             // 
             // numericUpDownRow
@@ -42,12 +42,12 @@
             this.numericUpDownRow.Size = new System.Drawing.Size(120, 20);
             this.numericUpDownRow.TabIndex = 0;
             // 
-            // numericUpDown1
+            // numericUpDownWin
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(209, 12);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDownWin.Location = new System.Drawing.Point(209, 12);
+            this.numericUpDownWin.Name = "numericUpDownWin";
+            this.numericUpDownWin.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownWin.TabIndex = 1;
             // 
             // buttonStart
             // 
@@ -65,12 +65,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.buttonStart);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.numericUpDownWin);
             this.Controls.Add(this.numericUpDownRow);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRow)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWin)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -78,7 +79,7 @@
         #endregion
 
         private System.Windows.Forms.NumericUpDown numericUpDownRow;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDownWin;
         private System.Windows.Forms.Button buttonStart;
     }
 }
--- piskvorky/Form1.cs
+++ piskvorky/Form1.cs
@@ -16,10 +16,44 @@ namespace piskvorky
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
-        private void buttonStart_Click(object sender, EventArgs e)
+
+        }
+    
+    
+
+        private void Delej(int x_tar, int y_tar)
+        {
+            int width = 20;
+            int height = 20;
+            int left = 30;
+            Button[,] buttonArray = new Button[x_tar,y_tar];
+            for (int x = 0; x < x_tar-1; x++)
+            {
+                for (int y = 0; y < y_tar-1; y++)
+                {
+                    buttonArray[x,y] = new Button();
+                    buttonArray[x, y].Name = $"btn_{x}_{y}";
+                    buttonArray[x,y].Size = new Size(width, height); 
+                    buttonArray[x,y].Location = new Point(20, 20);
+                    buttonArray[x,y].Click += new System.EventHandler(button_Click); this.Controls.Add(buttonArray[x,y]);
+                    left += 56;
+                }
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
         {
 
         }
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            Delej((int)numericUpDownRow.Value, (int)numericUpDownRow.Value);
+            Console.WriteLine("done");
+        }
+
+
     }
 }
